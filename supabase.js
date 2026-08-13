a925271f46af2d3c7ef2dfadcb81074722d6c6f8
almorpartners <almorpartners@gmail.com>
2026-08-13 10:55:22 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
-
-export const AUTH_EMAIL = import.meta.env.VITE_AUTH_EMAIL;
